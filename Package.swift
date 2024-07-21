0111089a0b05aa7c41b925fa9b9313948edcddfd
Aziz <abdulaziz@beinmedia.com>
2024-07-21 16:37:24 +0300

    MM3

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .binaryTarget(
       name: "ClashKit",
       url: "https://github.com/marab2/ClashKit/releases/download/untagged-2e7d218b62f534c935e4/ClashKit.xcframework.zip",
-      //checksum: "f8b8ee5b71b59053d156a3905df838e71ac434102f76a2797396bd6232c8b8df"
+      checksum: "f8b8ee5b71b59053d156a3905df838e71ac434102f76a2797396bd6232c8b8df"
     )
   ]
 )
